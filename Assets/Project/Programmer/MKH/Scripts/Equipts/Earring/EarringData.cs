3b84a0ff0c6b923738cb66647c3f4046b4bbc255
MoonKyoungHun <lsf8355@gmail.com>
2025-04-03 02:34:09 +0900

    [Fix] 장비 코드 수정
    아이템 코드 정리
    장비 스텟 하나로 통일되는 것 수정

--- Assets/Project/Programmer/MKH/Scripts/Equipts/Earring/EarringData.cs
+++ Assets/Project/Programmer/MKH/Scripts/Equipts/Earring/EarringData.cs
@@ -1,105 +1,102 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+
 namespace MKH
 {
     [CreateAssetMenu(fileName = "Earring_", menuName = "Add Item/Equipment_Earring")]
-    public class EarringData : Item_Equipment
+    public class EarringData : Item
     {
         public override Item Create()
         {
-            Item_Equipment createitem = Instantiate(this);
-            if (ItemType.Earring == Type)
+            Item item = Instantiate(this);
+
+            // 장비 등급을 숫자로 변환
+            // Normal : 0
+            // Magic : 1
+            // Rare : 2
+            int rate = (int)Rate;
+            var csv = CsvManager.Instance.earring;
+
+            float.TryParse(csv.GetData($"{rate}", "최소"), out float min);
+            float.TryParse(csv.GetData($"{rate}", "이속"), out float speed);
+            int.TryParse(csv.GetData($"{rate}", "체력"), out int hp);
+            int.TryParse(csv.GetData($"{rate}", "방어력"), out int defense);
+            int.TryParse(csv.GetData($"{rate}", "치확"), out int critical);
+            float.TryParse(csv.GetData($"{rate}", "공속"), out float attackSpeed);
+            int.TryParse(csv.GetData($"{rate}", "스테미나"), out int stemina);
+            float.TryParse(csv.GetData($"{rate}", "장비획득률 증가"), out float equipRate);
+            float.TryParse(csv.GetData($"{rate}", "공격력"), out float damage);
+            int.TryParse(csv.GetData($"{rate}", "마나"), out int mana);
+
+            // 장비 등급 숫자로 주스텟과 설명 추가
+            // 주스텟
+            item.mEffect.EquipRate = Mathf.Round(Random.Range(min, equipRate) * 100f) / 100f;
+            // 설명
+            item.Name = csv.GetData($"{rate}", "이름");
+            item.Description = $"장비 획득률 + {item.mEffect.EquipRate * 100f}%";
+
+            // 부스텟을 모두 0으로 변경
+            item.mEffect.HP = 0;
+            item.mEffect.Defense = 0;
+            item.mEffect.Critical = 0;
+            item.mEffect.AttackSpeed = 0;
+            item.mEffect.Stemina = 0;
+            item.mEffect.Damage = 0;
+            item.mEffect.Speed = 0;
+            item.mEffect.Mana = 0;
+
+            // 장비 등급 숫자만큼 랜덤한 부스텟 추가
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            for (int i = 0; i < rate; i++)  // Noraml : 0회 반복, Magic : 1회 반복, Rare : 2회 반복
             {
-                // 장비 등급을 숫자로 변환
-                // Normal : 0
-                // Magic : 1
-                // Rare : 2
-                int rate = (int)Rate;
+                int select = list[Random.Range(0, list.Count)];
+                list.Remove(select);
 
-                float.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "최소"), out float min);
-                float.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "이속"), out float speed);
-                int.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "체력"), out int hp);
-                int.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "방어력"), out int defense);
-                int.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "치확"), out int critical);
-                float.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "공속"), out float attackSpeed);
-                int.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "스테미나"), out int stemina);
-                float.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "장비획득률 증가"), out float equipRate);
-                float.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "공격력"), out float damage);
-                int.TryParse(CsvManager.Instance.earring.GetData($"{rate}", "마나"), out int mana);
-
-                // 장비 등급 숫자로 주스텟과 설명 추가
-                // 주스텟
-                createitem.mEffect.EquipRate = Mathf.Round(Random.Range(min, equipRate) * 100f) / 100f;
-                // 설명
-                createitem.Name = CsvManager.Instance.earring.GetData($"{rate}", "이름");
-                createitem.Description = $"장비 획득률 + {(createitem.mEffect.EquipRate * 100f).ToString()}%";
-
-                // 부스텟을 모두 0으로 변경
-                createitem.mEffect.HP = 0;
-                createitem.mEffect.Defense = 0;
-                createitem.mEffect.Critical = 0;
-                createitem.mEffect.AttackSpeed = 0;
-                createitem.mEffect.Stemina = 0;
-                createitem.mEffect.Damage = 0;
-                createitem.mEffect.Speed = 0;
-                createitem.mEffect.Mana = 0;
-
-                // 장비 등급 숫자만큼 랜덤한 부스텟 추가
-                List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
-                for (int i = 0; i < rate; i++)  // Noraml : 0회 반복, Magic : 1회 반복, Rare : 2회 반복
+                switch (select)
                 {
-                    int select = list[Random.Range(0, list.Count)];
-                    list.Remove(select);
-
-                    switch (select)
-                    {
-                        case 0:
-                            createitem.mEffect.HP = hp;
-                            createitem.Description += $"\n체력 + {createitem.mEffect.HP.ToString()}";
-                            break;
-                        case 1:
-                            createitem.mEffect.Defense = defense;
-                            createitem.Description += $"\n방어력 + {createitem.mEffect.Defense.ToString()}";
-                            break;
-                        case 2:
-                            createitem.mEffect.Critical = critical;
-                            createitem.Description += $"\n치명타 확률 + {createitem.mEffect.Critical.ToString()}%";
-                            break;
-                        case 3:
-                            createitem.mEffect.AttackSpeed = attackSpeed;
-                            createitem.Description += $"\n공격속도 + {(createitem.mEffect.AttackSpeed * 100f).ToString()}%";
-                            break;
-                        case 4:
-                            createitem.mEffect.Stemina = stemina;
-                            createitem.Description += $"\n스테미나 + {createitem.mEffect.Stemina.ToString()}";
-                            break;
-                        case 5:
-                            if (rate == 1)
-                            {
-                                createitem.mEffect.Damage = damage;
-                                createitem.Description += $"\n공격력 + {createitem.mEffect.Damage.ToString()}";
-                            }
-                            else if (rate == 2)
-                            {
-                                createitem.mEffect.Damage = (int)damage;
-                                createitem.Description += $"\n공격력 + {createitem.mEffect.Damage.ToString()}";
-                            }
-                            break;
-                        case 6:
-                            createitem.mEffect.Speed = speed;
-                            createitem.Description += $"\n이동속도 + {(createitem.mEffect.Speed * 100f).ToString()}";
-                            break;
-                        case 7:
-                            createitem.mEffect.Mana = mana;
-                            createitem.Description += $"\n마나 + {createitem.mEffect.Mana.ToString()}";
-                            break;
-                    }
-
+                    case 0:
+                        item.mEffect.HP = hp;
+                        item.Description += $"\n체력 + {hp}";
+                        break;
+                    case 1:
+                        item.mEffect.Defense = defense;
+                        item.Description += $"\n방어력 + {defense}";
+                        break;
+                    case 2:
+                        item.mEffect.Critical = critical;
+                        item.Description += $"\n치명타 확률 + {critical}%";
+                        break;
+                    case 3:
+                        item.mEffect.AttackSpeed = attackSpeed;
+                        item.Description += $"\n공격속도 + {(attackSpeed * 100f)}%";
+                        break;
+                    case 4:
+                        item.mEffect.Stemina = stemina;
+                        item.Description += $"\n스테미나 + {stemina}";
+                        break;
+                    case 5:
+                        if (rate == 1)
+                        {
+                            item.mEffect.Damage = damage;
+                            item.Description += $"\n공격력 + {damage}";
+                        }
+                        else if (rate == 2)
+                        {
+                            item.mEffect.Damage = (int)damage;
+                            item.Description += $"\n공격력 + {(int)damage}";
+                        }
+                        break;
+                    case 6:
+                        item.mEffect.Speed = speed;
+                        item.Description += $"\n이동속도 + {speed * 100}";
+                        break;
+                    case 7:
+                        item.mEffect.Mana = mana;
+                        item.Description += $"\n마나 + {mana}";
+                        break;
                 }
             }
-
-            return createitem;
+            return item;
         }
     }
 }
--- Assets/Project/Programmer/MKH/Scripts/Equipts/Helmet/HelmetData.cs
+++ Assets/Project/Programmer/MKH/Scripts/Equipts/Helmet/HelmetData.cs
@@ -4,102 +4,100 @@ using UnityEngine;
 namespace MKH
 {
     [CreateAssetMenu(fileName = "Helmet_", menuName = "Add Item/Equipment_Helmet")]
-    public class HelmetData : Item_Equipment
+    public class HelmetData : Item
     {
         public override Item Create()
         {
-            Item_Equipment createitem = Instantiate(this);
+            Item item = Instantiate(this);
 
-            if (ItemType.Helmet == Type)
+            // 장비 등급을 숫자로 변환
+            // Normal : 0
+            // Magic : 1
+            // Rare : 2
+            int rate = (int)Rate;
+            var csv = CsvManager.Instance.helmet;
+
+            int.TryParse(csv.GetData($"{rate}", "최소"), out int min);
+            float.TryParse(csv.GetData($"{rate}", "이속"), out float speed);
+            int.TryParse(csv.GetData($"{rate}", "체력"), out int hp);
+            int.TryParse(csv.GetData($"{rate}", "방어력"), out int defense);
+            int.TryParse(csv.GetData($"{rate}", "치확"), out int critical);
+            float.TryParse(csv.GetData($"{rate}", "공속"), out float attackSpeed);
+            int.TryParse(csv.GetData($"{rate}", "스테미나"), out int stemina);
+            float.TryParse(csv.GetData($"{rate}", "장비획득률 증가"), out float equipRate);
+            float.TryParse(csv.GetData($"{rate}", "공격력"), out float damage);
+            int.TryParse(csv.GetData($"{rate}", "마나"), out int mana);
+
+            // 장비 등급 숫자로 주스텟과 설명 추가
+            // 주스텟
+            item.mEffect.HP = Random.Range(min, hp);
+            // 설명
+            item.Name = csv.GetData($"{rate}", "이름");
+            item.Description = $"체력 + {item.mEffect.HP}";
+
+            // 부스텟을 모두 0으로 변경
+            item.mEffect.Defense = 0;
+            item.mEffect.Critical = 0;
+            item.mEffect.AttackSpeed = 0;
+            item.mEffect.Stemina = 0;
+            item.mEffect.EquipRate = 0;
+            item.mEffect.Damage = 0;
+            item.mEffect.Speed = 0;
+            item.mEffect.Mana = 0;
+
+            // 장비 등급 숫자만큼 랜덤한 부스텟 추가
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            for (int i = 0; i < rate; i++)  // Noraml : 0회 반복, Magic : 1회 반복, Rare : 2회 반복
             {
-                // 장비 등급을 숫자로 변환
-                // Normal : 0
-                // Magic : 1
-                // Rare : 2
-                int rate = (int)Rate;
+                int select = list[Random.Range(0, list.Count)];
+                list.Remove(select);
 
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "최소"), out int min);
-                float.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "이속"), out float speed);
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "체력"), out int hp);
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "방어력"), out int defense);
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "치확"), out int critical);
-                float.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "공속"), out float attackSpeed);
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "스테미나"), out int stemina);
-                float.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "장비획득률 증가"), out float equipRate);
-                float.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "공격력"), out float damage);
-                int.TryParse(CsvManager.Instance.helmet.GetData($"{rate}", "마나"), out int mana);
-
-                // 장비 등급 숫자로 주스텟과 설명 추가
-                // 주스텟
-                createitem.mEffect.HP = Random.Range(min, hp);
-                // 설명
-                createitem.Name = CsvManager.Instance.helmet.GetData($"{rate}", "이름");
-                createitem.Description = $"체력 + {createitem.mEffect.HP.ToString()}";
-
-                // 부스텟을 모두 0으로 변경
-                createitem.mEffect.Defense = 0;
-                createitem.mEffect.Critical = 0;
-                createitem.mEffect.AttackSpeed = 0;
-                createitem.mEffect.Stemina = 0;
-                createitem.mEffect.EquipRate = 0;
-                createitem.mEffect.Damage = 0;
-                createitem.mEffect.Speed = 0;
-                createitem.mEffect.Mana = 0;
-
-                // 장비 등급 숫자만큼 랜덤한 부스텟 추가
-                List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
-                for (int i = 0; i < rate; i++)  // Noraml : 0회 반복, Magic : 1회 반복, Rare : 2회 반복
+                switch (select)
                 {
-                    int select = list[Random.Range(0, list.Count)];
-                    list.Remove(select);
-
-                    switch (select)
-                    {
-                        case 0:
-                            createitem.mEffect.Defense = defense;
-                            createitem.Description += $"\n방어력 + {createitem.mEffect.Defense.ToString()}";
-                            break;
-                        case 1:
-                            createitem.mEffect.Critical = critical;
-                            createitem.Description += $"\n치명타 확률 + {createitem.mEffect.Critical.ToString()}%";
-                            break;
-                        case 2:
-                            createitem.mEffect.AttackSpeed = attackSpeed;
-                            createitem.Description += $"\n공격속도 + {(createitem.mEffect.AttackSpeed * 100f).ToString()}%";
-                            break;
-                        case 3:
-                            createitem.mEffect.Stemina = stemina;
-                            createitem.Description += $"\n스테미나 + {createitem.mEffect.Stemina.ToString()}";
-                            break;
-                        case 4:
-                            createitem.mEffect.EquipRate = equipRate;
-                            createitem.Description += $"\n장비 획득률 + {(createitem.mEffect.EquipRate * 100f).ToString()}%";
-                            break;
-                        case 5:
-                            if (rate == 1)
-                            {
-                                createitem.mEffect.Damage = damage;
-                                createitem.Description += $"\n공격력 + {createitem.mEffect.Damage.ToString()}";
-                            }
-                            else if (rate == 2)
-                            {
-                                createitem.mEffect.Damage = (int)damage;
-                                createitem.Description += $"\n공격력 + {createitem.mEffect.Damage.ToString()}";
-                            }
-                            break;
-                        case 6:
-                            createitem.mEffect.Speed = speed;
-                            createitem.Description += $"\n이동속도 + {(createitem.mEffect.Speed * 100f).ToString()}";
-                            break;
-                        case 7:
-                            createitem.mEffect.Mana = mana;
-                            createitem.Description += $"\n마나 + {createitem.mEffect.Mana.ToString()}";
-                            break;
-                    }
-
+                    case 0:
+                        item.mEffect.Defense = defense;
+                        item.Description += $"\n방어력 + {defense}";
+                        break;
+                    case 1:
+                        item.mEffect.Critical = critical;
+                        item.Description += $"\n치명타 확률 + {critical}%";
+                        break;
+                    case 2:
+                        item.mEffect.AttackSpeed = attackSpeed;
+                        item.Description += $"\n공격속도 + {attackSpeed * 100f}%";
+                        break;
+                    case 3:
+                        item.mEffect.Stemina = stemina;
+                        item.Description += $"\n스테미나 + {stemina}";
+                        break;
+                    case 4:
+                        item.mEffect.EquipRate = equipRate;
+                        item.Description += $"\n장비 획득률 + {equipRate * 100f}%";
+                        break;
+                    case 5:
+                        if (rate == 1)
+                        {
+                            item.mEffect.Damage = damage;
+                            item.Description += $"\n공격력 + {damage}";
+                        }
+                        else if (rate == 2)
+                        {
+                            item.mEffect.Damage = (int)damage;
+                            item.Description += $"\n공격력 + {(int)damage}";
+                        }
+                        break;
+                    case 6:
+                        item.mEffect.Speed = speed;
+                        item.Description += $"\n이동속도 + {speed * 100f}";
+                        break;
+                    case 7:
+                        item.mEffect.Mana = mana;
+                        item.Description += $"\n마나 + {mana}";
+                        break;
                 }
             }
-            return createitem;
+            Debug.Log(item.mEffect.HP);
+            return item;
         }
 
     }
