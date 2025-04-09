be19f6bbd8f42f5e95c048e7530ae574f111b08c
MoonKyoungHun <lsf8355@gmail.com>
2025-04-10 05:13:08 +0900

    [Refactor] 인벤토리 리펙토링
    필요 없는 코드 제거
    주석 추가 및 수정
    Sorting() 코드 수정

--- Assets/Project/Programmer/MKH/Scripts/Inventory/InventoryMain.cs
+++ Assets/Project/Programmer/MKH/Scripts/Inventory/InventoryMain.cs
@@ -2,23 +2,31 @@ using UnityEngine;
 
 namespace MKH
 {
+    /// <summary>
+    /// 인벤토리의 주요 동작을 처리하는 클래스. 
+    /// 아이템 획득, 슬롯 정렬 등의 기능을 담당합니다.
+    /// </summary>
     public class InventoryMain : InventoryBase
     {
-        [SerializeField] GameObject state;
-
+        /// <summary>
+        /// 부모 클래스의 Awake를 명시적으로 호출 (new 키워드로 숨김 처리됨).
+        /// </summary>
         new public void Awake()
         {
             base.Awake();
         }
 
+        /// <summary>
+        /// 아이템을 획득하여 인벤토리의 빈 슬롯에 추가합니다.
+        /// </summary>
         public void AcquireItem(Item item)
         {
-            Item _item = item.Create();
+            Item _item = item.Create(); // 아이템 복제
 
             for (int i = 0; i < mSlots.Length; i++)
             {
-                //아이템이 없을 시 넣기
-                if (mSlots[i].Item == null)
+                // 빈 슬롯이 있을 경우 아이템을 추가하고 종료
+                if (mSlots[i].IsEmpty())
                 {
                     mSlots[i].AddItem(_item);
                     return;
@@ -26,41 +34,36 @@ namespace MKH
             }
         }
 
+        /// <summary>
+        /// 아이템을 추가할 수 있는 빈 슬롯을 찾습니다.
+        /// </summary>
+        /// <returns>빈 슬롯이 있다면 해당 슬롯, 없다면 null 반환</returns>
         public InventorySlot IsCanAquireItem(Item item)
         {
-            foreach (InventorySlot slot in mSlots)
+            for (int i = 0; i < mSlots.Length; i++)
             {
-                if (slot.Item == null)
+                if (mSlots[i].IsEmpty())
                 {
-                    return slot;
+                    return mSlots[i];
                 }
             }
             return null;
         }
 
-        // 아이템 빈칸 없이 당기기
+        /// <summary>
+        /// 인벤토리 슬롯을 정렬하여 빈 칸 없이 아이템을 당깁니다.
+        /// 앞쪽이 비어 있고, 뒤에 아이템이 있으면 앞으로 이동시킴.
+        /// </summary>
         public void Sorting()
         {
             for (int i = 0; i < mSlots.Length - 1; i++)
             {
-                if (mSlots[i].Item == null)
+                // 앞 슬롯이 비어있고, 뒷 슬롯에 아이템이 있는 경우
+                if (mSlots[i].IsEmpty() && !mSlots[i + 1].IsEmpty())
                 {
-                    Color color1 = mSlots[i + 1].ItemImage.color;
-                    color1.a = 0f;
-
-                    mSlots[i].ItemImage.sprite = mSlots[i + 1].ItemImage.sprite;
-                    mSlots[i].ItemImage.color = mSlots[i + 1].ItemImage.color;
-                    mSlots[i].Item = mSlots[i + 1].Item;
-                    mSlots[i + 1].Item = null;
-                    mSlots[i + 1].ItemImage.color = color1;
-                    mSlots[i + 1].ItemImage.sprite = null;
-
-                    if (mSlots[i].ItemImage.sprite == null)
-                    {
-                        Color color = mSlots[i].ItemImage.color;
-                        color.a = 0;
-                        mSlots[i].ItemImage.color = color;
-                    }
+                    // 뒷 슬롯의 아이템을 앞 슬롯으로 옮기고, 뒷 슬롯은 비움
+                    mSlots[i].AddItem(mSlots[i + 1].Item);
+                    mSlots[i + 1].ClearSlot();
                 }
             }
         }
